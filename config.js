165d5b68a8292b93b27b05449af73caa1012f8ae
andreyzadaa <andreypereira831@gmail.com>
2026-01-10 23:28:32 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+document.getElementById("tb").onclick = function () {
+    alert("She just let me know\n\nWhere they show no fear\n\nIt ain't safe 'round here\n\nBut she still gonna hold on\n\nIt ain't safe 'round here\n\nBut she doesn't run from right here\n\nLust, guns, drugs, violence\n\nLust, guns, drugs, violence, ooh")
+
+};
+
+document.getElementById("lwy").onclick = function () {
+    alert("Sometimes I wish you knew\n\nBut I disguise the truth\n\nI say I'm happy but I'm still stuck on us, mmm, mmm, mmm\n\nDoes your mind play this game too?\n\nThink 'bout me and you\n\nI guess I'll just pretend\n\nUntil it all makes sense, hmm, hmm, hmm, hmm")
+}
+
+document.getElementById("ha").onclick = function () {
+    alert("Ooh\n\nBalloons are deflated\n\nGuess they look lifeless like me\n\nWe miss you on your side of the bed, mmm\n\nStill got your things here\n\nAnd they stare at me like souvenirs\n\nDon't wanna let you out my head")
+}
